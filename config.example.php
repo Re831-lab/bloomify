7dab9df6530801e079f0a011f186ef6cc56fc254
Re <rasha.2005zreaq@gmail.com>
2026-03-23 00:00:23 +0200

    Change password to placeholder in config example
    Updated password placeholder in configuration file.

--- config.example.php
+++ config.example.php
@@ -1,7 +1,9 @@
 <?php
+// Database Configuration Example
+// Copy this file to config.php and update with your credentials
 $host = "mysql.railway.internal";
 $user = "root";
-$password = "gtnpleRHzzEcNnnxhugIBarJSvORsrVW";
+$password = "YOUR_PASSWORD_HERE";
 $db = "railway";
 $conn = new mysqli($host, $user, $password, $db);
 if($conn->connect_error)
